e30a231c569efd03c9ec35d37d47d13f222a952a
sodrooome <15523163@students.uii.ac.id>
2020-12-22 12:15:32 +0700

    style: fixed indentation and tab for mixing

--- dj/utils.py
+++ dj/utils.py
@@ -3,21 +3,22 @@ import sys
 
 
 class Script(object):
-	def __init__(self, name="manage.py") -> bool:
-		self.name = name
+    def __init__(self, name="manage.py") -> bool:
+        self.name = name
 
-	def find_script(self) -> bool:
-		"""Method for searching manage.py file"""
-		paths = []
-		directory = os.getcwd()
-		while True:
-			django_path = os.path.join(directory, self.name)
-			paths.append(django_path)
-			if os.path.exists(django_path):
-				return django_path
+    def find_script(self) -> bool:
+        """Method for searching manage.py file"""
+        paths = []
+        directory = os.getcwd()
+        while True:
+            django_path = os.path.join(directory, self.name)
+            paths.append(django_path)
+            if os.path.exists(django_path):
+                return django_path
+                break
 
-			django_base_path = os.path.dirname(directory)
-			if django_base_path == directory:
-				# why this exit doesn't work?
-				sys.exit("No manage.py file or something like Django environment is exists in here.")
-			directory = django_base_path
+            django_base_path = os.path.dirname(directory)
+            if django_base_path == directory:
+                # why this exit doesn't work?
+                sys.exit("No manage.py file or something like Django environment is exists in here.")
+            directory = django_base_path
