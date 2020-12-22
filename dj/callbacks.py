780c74cec58802123f935a88d0dca1eb22e0f023
sodrooome <15523163@students.uii.ac.id>
2020-12-22 12:14:42 +0700

    chore: import get_version method into versioning

--- dj/callbacks.py
+++ dj/callbacks.py
@@ -1,13 +1,16 @@
 import click
+from .version import get_version
+
 
 class Versioning(object):
 	def __init__(self, ctx, param, value):
-		self.ctx = ctx
-		self.param = param
-		self.value = value
+	    self.ctx = ctx
+	    self.param = param
+	    self.value = value
 
+	@property
 	def version(self):
 		if not self.value or self.ctx.resilient_parsing:
 			return
-		click.echo("Django CLI v0.1.0")
+		click.echo(get_version())
 		self.ctx.exit()
